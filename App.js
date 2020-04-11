67662d18d5fae6086a580caf8936e25ceaea5b5d
prasanna bisen <prasannabisen@gmail.com>
2020-04-11 18:24:18 +0530

    .

--- App.js
+++ App.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name,setName]=useState('shaun')
+  const [person,setPerson]=useState({name:"Montu",age:"20"})
+  const clickHandler=()=>{
+    setName('montu');
+    setPerson({name:"prasanna",age:"22"});
+  }
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.text}>hello world</Text>
-        <Text style={styles.text}>hello world 2</Text>
-        <Text style={styles.text}>hello world 3</Text>
-        <Text style={styles.text}>hello world 4</Text>
+      <Text>My Name is:-{name}</Text> 
+      <Text>My real name is:- {person.name} and age</Text>
+      <View style={styles.buttonk}>
+        <Button title='update state' onPress={clickHandler}/>
       </View>
-      <View style={styles.body}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({  
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header:{
-    backgroundColor:'red',
-    padding:20,
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
   },
   text:{
-    fontWeight:'bold',
-    fontSize:30,
+    backgroundColor:'#eee',
+    margin:5,
+    fontSize:40,
+    borderRadius:10,
+    overflow:'hidden',
+    width:400
   },
-  body:{
-    backgroundColor:'pink',
-    padding:20,
+  buttonk:{
+    marginTop:20,
   }
 });
