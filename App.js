f9d45e0867327efca577606d3327e59ae2453da0
prasanna bisen <prasannabisen@gmail.com>
2020-04-13 15:59:32 +0530

    .

--- App.js
+++ App.js
@@ -1,44 +1,8 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, Button, FlatList,TouchableOpacity} from 'react-native';
+import { StyleSheet} from 'react-native';
 
-export default function App() {
-  const [people,setPeople]=useState([
-    {name:'montu',key:1},
-    {name:'prasanna',key:2},
-    {name:'himanshu',key:3},
-    {name:'navneet',key:4},
-    {name:'prashant',key:5},
-  ]);
+export default function App(){
 
-  const pressHandler=(key)=>{
-    console.log(key);
-    setPeople((prevPeople)=>{
-      return prevPeople.filter(person=>person.key !=key);
-    })
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList 
-      data={people} 
-      renderItem={({item})=>(
-        <TouchableOpacity onPress={()=>pressHandler(item.key)}>
-          <Text style={styles.item}>{item.name}</Text>
-        </TouchableOpacity>
-      )}
-      />
-
-      {/* <ScrollView>
-      {people.map(item=>{
-        return(
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-        )
-      })}
-      </ScrollView> */}
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({  
@@ -48,10 +12,4 @@ const styles = StyleSheet.create({
     //alignItems:'center',
     //justifyContent:'center',
   },
-  item:{
-    backgroundColor:'pink',
-    margin:30,
-    marginTop:24,
-    fontSize:34,
-  }
 });
