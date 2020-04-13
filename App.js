e995e2f3ad5be7cb5ccb8a9221233d598b4dd5a2
prasanna bisen <prasannabisen@gmail.com>
2020-04-13 14:29:20 +0530

    .

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ export default function App() {
   ]);
   return (
     <View style={styles.container}>
-
       <FlatList data={people} renderItem={({item})=>(
         <Text style={styles.item}>{item.name}</Text>
       )}
