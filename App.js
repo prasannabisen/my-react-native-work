c81003b4824c9694e873cadc8bacdcc5f8ef78ce
prasanna bisen <prasannabisen@gmail.com>
2020-04-13 16:34:43 +0530

    .

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import React,{useState} from 'react';
-import { StyleSheet} from 'react-native';
+import { StyleSheet, View,Text,FlatList} from 'react-native';
 
 export default function App(){
+  const [todos,setTodos]=useState([
+    {text:"buy coffee",key:'1'},
+    {text:"create app",key:'2'},
+    {text:"play with phone",key:'3'}
+  ]);
 
+  return(
+    <View style={styles.container}>
+      {/* Header*/}
+      <View>
+        {/*TO do form*/}
+        <View style={styles.list}>
+          {/*to do list*/}
+          <FlatList 
+            data={todos}
+            renderItem={({item})=>(
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({  
